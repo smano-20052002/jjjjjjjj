f671f708c0c36f1d370305310b266f2096f6ccac
Gokuljawahar <gokuleduid@gmail.com>
2024-06-18 10:10:39 +0530

    Added additional functionality to get the feedback details based on quiz topic and learner id

--- LXP.API/obj/Debug/net8.0/LXP.Api.AssemblyInfo.cs
+++ LXP.API/obj/Debug/net8.0/LXP.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LXP.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7791a57a5a6663763176ed96be7f04ae2a60c42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LXP.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LXP.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
